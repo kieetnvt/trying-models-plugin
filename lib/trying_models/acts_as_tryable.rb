8a4ffac0f473cbcbd64ce5eace34db60356c3e18
Kiet Nguyen <kietdev38@gmail.com>
2016-04-05 23:30:09 +0700

    understanding the extend and the include with active support concern

--- lib/trying_models/acts_as_tryable.rb
+++ lib/trying_models/acts_as_tryable.rb
@@ -2,14 +2,24 @@ module TryingModels
   module ActsAsTryable
     extend ActiveSupport::Concern
 
-    included do
+    def self.included(target)
+      target.send :include, LocalInstanceMethods
+      target.send :extend, ClassMethods
     end
 
     module ClassMethods
       def acts_as_tryable
+        include TryingModels::ActsAsTryable::LocalInstanceMethods
         return "say hello with acts_as_tryable intergate with model"
       end
     end
+
+    module LocalInstanceMethods
+      def full_content
+        return "name is '#{name}' and content is '#{content}'"
+      end
+    end
+
   end
 end
 
